a7453529c29fc0c95869f7256a43732a95d17b63
Delton Ding <dsh0416@gmail.com>
2026-08-01 21:31:28 +0800

    drop screenshot tests

--- apps/design-system/playwright.config.ts
+++ apps/design-system/playwright.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, devices } from "@playwright/test"
 export default defineConfig({
   testDir: "./tests",
   outputDir: "./test-results",
-  snapshotPathTemplate: "{testDir}/__screenshots__/{arg}{ext}",
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
